295bb330896f526c701675cb2655ff6bd92f10d5
松本 泰典 <matsumoto@caters.co.jp>
2015-05-26 09:42:26 +0900

    add browserSync : at gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var IS_MIN = false;
 var JS_MIN = true;
-var IS_LOCAL = true;
 
 var dir = {
   src  : 'src/',
@@ -20,7 +19,7 @@ var gulpif         = require('gulp-if');
 var useref         = require('gulp-useref');
 var rename         = require('gulp-rename');
 var replace        = require('gulp-replace');
-var connect = require('gulp-connect');
+var browserSync    = require('browser-sync');
 
 //js
 var typescript     = require('gulp-typescript');
@@ -32,30 +31,23 @@ var less           = require('gulp-less');
 var pleeease       = require('gulp-pleeease');
 
 //html
-var jade           = require('gulp-jade');
-var htmlmin        = require('gulp-minify-html');
+var jade        = require('gulp-jade');
 
 //image
-var imagemin       = require('gulp-imagemin');
-var pngquant       = require('imagemin-pngquant');
+var imagemin    = require('gulp-imagemin');
+var pngquant    = require('imagemin-pngquant');
+var spritesmith = require('gulp.spritesmith');
 
-
-//  Bower Settings  ----------------------------------
-gulp.task('clear-libs', function() {
-    del.sync(dir.dest + 'lib/');
-});
 
 gulp.task('bower', function() {
   gulp.src(mainBowerFiles({debugging:true, checkExistence:true}))
   .pipe(concat('libs.js'))
-  .pipe(uglify())
   .pipe(gulp.dest(dir.src + 'common/js'));
 });
 
 //  copy_defaut  ----------------------------------
 gulp.task('copy-static', function() {
   gulp.src([dir.src + '**/*.css', dir.src + '**/*.inc', dir.src + '**/*.js', dir.src + '**/*.def', dir.src + '**/*.xml', dir.src + '**/*.mp4'])
-    .pipe(changed( dir.dest ))
     .pipe(gulp.dest(dir.dest));
 });
 
@@ -73,7 +65,8 @@ gulp.task('less', function() {
       minifier: IS_MIN
     }))
     .pipe(gulpif(!IS_MIN, replace(/  /g, '\t')))
-    .pipe(gulp.dest(dir.dest));
+    .pipe(gulp.dest(dir.dest))
+    .pipe(browserSync.reload({stream: true}));
 });
 
 //  sass  ----------------------------------
@@ -82,7 +75,10 @@ gulp.task('sass', function(){
   .pipe(changed( dir.dest ))
   .pipe(plumber())
   .pipe(sass({
-    outputStyle:'nested'
+    // outputStyle:'nested'
+    // outputStyle:'expanded'
+    outputStyle:'compact'
+    // outputStyle:'compressed'
   }))
   .pipe(pleeease({
     autoprefixer: {
@@ -90,9 +86,10 @@ gulp.task('sass', function(){
     },
     minifier: IS_MIN
   }))
+  .pipe(gulpif(!IS_MIN, replace(/\n\n/g, '\n')))
   .pipe(gulpif(!IS_MIN, replace(/  /g, '\t')))
   .pipe(gulp.dest(dir.dest))
-  .pipe(gulpif(IS_LOCAL, connect.reload()));
+  .pipe(browserSync.reload({stream: true}));
 });
 
 //  jade  ----------------------------------
@@ -105,7 +102,7 @@ gulp.task('jade', function () {
     }))
     .pipe(replace(/  /g, '\t'))
     .pipe(gulp.dest(dir.dest))
-    .pipe(gulpif(IS_LOCAL, connect.reload()));
+    .pipe(browserSync.reload({stream: true}));
 });
 
 //image ----------------------------------
@@ -121,8 +118,31 @@ gulp.task( 'imagemin', function(){
 
   gulp.src( dir.src + '/**/*.+(jpg|jpeg|png|gif|svg)' )
     .pipe(changed( dir.dest ))
-    .pipe(imagemin())
+    .pipe(imagemin(imageminOptions))
     .pipe(gulp.dest( dir.dest ));
+});
+
+gulp.task('sprite', function () {
+  var _source = dir.src + '/twc/info/super/img/utils/';
+  var _imgOutput = dir.src + '/twc/info/super/img/sprites/';
+  var _scssOutput = dir.src + '/twc/info/super/css/';
+
+  var spriteData = gulp.src(_source + '*.png') //スプライトにする愉快な画像達
+  .pipe(spritesmith({
+    imgName: 'sprite.png', //スプライトの画像
+    cssName: '_sprite.scss', //生成されるscss
+    imgPath: '../img/sprites/sprite.png', //生成されるscssに記載されるパス
+    cssFormat: 'scss', //フォーマット
+    padding : 10,
+    cssVarMap: function (sprite) {
+      sprite.name = sprite.name; //VarMap(生成されるScssにいろいろな変数の一覧を生成)
+    },
+    cssOpts: {
+      functions: false
+    }
+  }));
+  spriteData.img.pipe(gulp.dest(_imgOutput)); //imgNameで指定したスプライト画像の保存先
+  spriteData.css.pipe(gulp.dest(_scssOutput)); //cssNameで指定したcssの保存先
 });
 
 //  js  ----------------------------------
@@ -132,7 +152,8 @@ var typescriptProject = typescript.createProject({
   sortOutput     : true,
   noImplicitAny  : false,
   noEmitOnError  : false,
-  module         :"commonjs"
+  module         :"commonjs",
+  typescript: require('typescript')
 });
 
 gulp.task('typescript', function(){
@@ -142,16 +163,12 @@ gulp.task('typescript', function(){
     .js
     .pipe(gulpif(JS_MIN, uglify()))
     .pipe(gulpif(!JS_MIN, replace(/    /g, '\t')))
-    .pipe(gulp.dest(dir.dest));
+    .pipe(gulp.dest(dir.dest))
+    .pipe(gulpif(IS_LOCAL, browserSync.reload({stream: true})));
 });
 
-//- ----------------------------------------------------------- <
-gulp.task('connectDev',function(){
-  connect.server({
-    root: [dir.dest],   //ルートディレクトリ
-    port: 8000,     //ポート番号
-    livereload: true
-  });
+gulp.task('bs-reload', function () {
+  browserSync.reload();
 });
 
 
@@ -166,10 +183,13 @@ gulp.task('publishJS', function() {
 
 //  watch  ----------------------------------
 gulp.task('watch', function () {
+    browserSync({
+      server: {
+        baseDir: dir.dest
+      }
+    });
     gulp.watch(dir.src + '**/*.scss', ['sass']);
     gulp.watch(dir.src + '**/*.less', ['less']);
     gulp.watch(dir.src + '**/*.jade', ['jade']);
     gulp.watch(dir.src + '**/*.ts', ['typescript']);
 });
-
-gulp.task('default',['watch','connectDev']);
